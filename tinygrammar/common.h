0bd184d35da77ad3853d5bd35e62197732216c02
Fabio Pellacini <fabio@Fabios-MacBook-Pro-15.local>
2017-04-21 19:35:04 +0200

    updated

--- tinygrammar/common.h
+++ tinygrammar/common.h
@@ -116,7 +116,8 @@ typedef ym_vec<int, TAG_SIZE> rule_tags;
 
 typedef ym_vec<double, PARAM_SIZE> anim_params;
 
-static string grammar_filename = "grammars/test_time_grammar_test_teaser.json";
+// static string grammar_filename = "grammars/test_time_grammar_test_teaser.json";
+static string grammar_filename = "grammars/test_edo_time_grammar_tris.json";
 
 // ==============================================
 // ====== METHODS FOR LOG/EXP OF MATRICES =======
--- tinygrammar/main_timeline.cpp
+++ tinygrammar/main_timeline.cpp
@@ -68,9 +68,10 @@ int main(int argc, const char * argv[]) {
     
     auto em = (HistoryAnim*)(make_history(animation_history));
     auto grammar = get_grammar(grammar_filename);
-    auto tree = initialize_tree(grammar, 3, 5, "resources/svg/teaser.svg");
+//    auto tree = initialize_tree(grammar, 3, 5, "resources/svg/teaser.svg");
 //    auto tree = initialize_tree(grammar, 2, 7, "resources/svg/test_tagged_2.svg"); // squares and circles
 //    auto tree = initialize_tree(grammar, 2, 2, "resources/svg/test_tagged_2.svg"); // squares and circles
+    auto tree = initialize_tree(grammar, 3, 5, "resources/svg/edo_square100.svg");
     
     auto init_step = matching_init();
     auto init_shapes = init_step->op(ShapeGroup(), init_step->produced_tags, init_step->parameters, grammar->rn, nullptr, nullptr, tree);
