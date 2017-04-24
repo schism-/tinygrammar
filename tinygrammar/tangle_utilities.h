5e0f167a83533d13eecc3add82243b681bc3446a
Fabio Pellacini <fabio@Fabios-MBP-15.station>
2017-04-24 13:42:47 +0200

    updated

--- tinygrammar/tangle_utilities.h
+++ tinygrammar/tangle_utilities.h
@@ -154,7 +154,14 @@ inline bool inside_polylines(const vector<polyline2r>& curves, const ym_vec2r& p
 }
 
 inline bool inside_polyline(const polyline2r& curve, const ym_vec2r& p) {
-    return inside_polylines({curve}, p);
+    auto flips = false;
+    for(auto i : range((int)curve.size()-1)) {
+        if ( ((curve[i].y>p.y) != (curve[i+1].y>p.y)) &&
+            (p.x < (curve[i].x-curve[i+1].x) * (p.y-curve[i].y) /
+             (curve[i+1].y-curve[i].y) + curve[i].x) )
+            flips = not flips;
+    }
+    return flips;
 }
 
 // only for closed ones
