6162a5d027f7a4d1c2b36f270dd9ae995496ce7b
Fabio Pellacini <fabio@Fabios-MBP-15.station>
2017-04-24 15:15:11 +0200

    updated

--- tinygrammar/csg_tree.cpp
+++ tinygrammar/csg_tree.cpp
@@ -184,8 +184,8 @@ void CSGTree::Update_Sum(CSGTree::Tree* tree, CSGTree::OpNode* node){
 #if SPEEDUP_ALIASSUM
     node->shapes.clear();
     node->shapes.reserve(a->shapes.size()+b->shapes.size());
-    for(auto s : a->shapes) node->shapes.push_back(s);
-    for(auto s : b->shapes) node->shapes.push_back(s);
+    node->shapes.insert(node->shapes.begin(), a->shapes.begin(), a->shapes.end());
+    node->shapes.insert(node->shapes.begin(), b->shapes.begin(), b->shapes.end());
     return;
 #endif
 
