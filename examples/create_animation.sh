ed06f047da7a73a354c6bdf5163d2f3ea0df1094
Edoardo Carra <edoardocarra@Edoardos-MBP.mshome.net>
2017-05-10 16:53:53 +0200

    updating results
    Signed-off-by: Edoardo Carra <edoardocarra@Edoardos-MBP.mshome.net>

--- examples/create_animation.sh
+++ examples/create_animation.sh
@@ -16,8 +16,8 @@ START_TIME=$SECONDS
 echo "Execution ended in : $(($SECONDS - $START_TIME)) seconds!"
 
 echo "CREATING PNG FRAMES"
-mogrify -format png $3/frames/*.svg
+mogrify -format png -density 20 $3/frames/*.svg
 
-#convert -density 40 -resize 500x500 $3/svgs/*.svg -set filename:base "%[base]" $3/pngs/"%[filename:base].png"
+#convert -density 20 -resize 500x500 $3/svgs/*.svg -set filename:base "%[base]" $3/pngs/"%[filename:base].png"
 echo "CREATING GIF"
 convert -delay 1/30 -loop 0 $3/frames/*.png $3/animated.gif
